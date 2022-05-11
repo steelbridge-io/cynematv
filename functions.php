1ec61bd24c310b381afc568fe0a1215992b2d003
Chris Parsons <chris@parsonshosting.com>
2022-05-10 21:31:17 -0700

    first commit

---
+++ functions.php
@@ -0,0 +1,78 @@
+<?php
+ 
+ /**
+	* Disable JetPack Upsells
+	*/
+ 
+ add_filter( 'jetpack_just_in_time_msgs', '_return_false' );
+
+// Include php files
+include get_theme_file_path('/includes/shortcodes.php');
+include get_theme_file_path('includes/custom-post-types/video.php');
+
+// Enqueue needed scripts
+function needed_styles_and_scripts_enqueue() {
+ 
+	 // Custom script
+ wp_enqueue_script( 'wpbs-custom-script', get_stylesheet_directory_uri() . '/assets/javascript/script.js' , array( 'jquery' ) );
+ 
+ 	// Video JS
+ wp_enqueue_script('video-js', get_stylesheet_directory_uri() . '/assets/javascript/video.js', array(), '', true );
+ 
+ // enqueue style
+ wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
+ // enqueue Lineicons
+ wp_register_style('lineicons', 'https://cdn.lineicons.com/3.0/lineicons.css', array(), '', 'all' );
+ wp_enqueue_style('lineicons');
+ 
+ // Add-ons
+ wp_enqueue_style('custom', get_stylesheet_directory_uri() . '/assets/scss/supports.css', array(), '', 'all' );
+ 
+ // Animate CSS
+ wp_register_style('animate-css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css', array(), '', 'all');
+ wp_enqueue_style('animate-css');
+ 
+}
+add_action( 'wp_enqueue_scripts', 'needed_styles_and_scripts_enqueue' );
+
+function cc_mime_types($mimes) {
+$mimes['svg'] = 'image/svg+xml';
+return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+
+add_filter( 'widget_text', 'do_shortcode' );
+
+//Dynamic Year
+function site_year(){
+	ob_start();
+	echo date( 'Y' );
+	$output = ob_get_clean();
+    return $output;
+}
+add_shortcode( 'site_year', 'site_year' );
+
+//
+// Your code goes below
+//
+	
+	function cynematv_add_woocommerce_support() {
+		add_theme_support( 'woocommerce', array(
+			'thumbnail_image_width' => 150,
+			'single_image_width'    => 300,
+			
+			'product_grid'          => array(
+				'default_rows'    => 3,
+				'min_rows'        => 2,
+				'max_rows'        => 8,
+				'default_columns' => 4,
+				'min_columns'     => 2,
+				'max_columns'     => 5,
+			),
+		) );
+	}
+	add_action( 'after_setup_theme', 'cynematv_add_woocommerce_support' );
+ 
+ remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
